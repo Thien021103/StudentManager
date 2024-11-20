e03c60bff7263ec460276587ba86ec0895ab8cfc
Thien021103 <thien881199@gmail.com>
2024-11-20 14:36:22 +0700

    main1

--- app/src/main/java/com/example/studentmanager/MainActivity.kt
+++ app/src/main/java/com/example/studentmanager/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
